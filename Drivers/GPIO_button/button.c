d06be517b55d292af6c00dff22e2086259580fa2
weinan9527 <edisonjwn@qq.com>
2013-10-20 19:20:34 +0200

    Adding IRQ into the original button driver

--- Drivers/GPIO_button/button.c
+++ Drivers/GPIO_button/button.c
@@ -18,7 +18,9 @@ MODULE_VERSION("0.1");
 #define CLASS_NAME "button1class"
 
 #define STATUS_BUFFER_SIZE 128
-#define GPIO_NUMBER 30
+#define GPIO_BUTTON1_NUMBER 30
+#define GPIO_LED1_NUMBER 60
+
 static int cnt = 0;
 unsigned char text[100];
 
@@ -49,11 +51,19 @@ static ssize_t button1_read( struct file* filp, char *user_buf, size_t count, lo
 	printk(KERN_INFO "READ FUNCTION CALLED!\n");
 	int status =0;
 
-	int button_value = gpio_get_value(30);
+	int button_value = gpio_get_value(GPIO_BUTTON1_NUMBER);
         printk(KERN_INFO "BUTTON VALUE = %d\n",button_value );
 	
-	if (button_value == 0)	sprintf(text,"Button is PRESSED\n");
-	else sprintf(text,"Button is RELEASED\n");
+	if (button_value == 0)
+	{
+		sprintf(text,"Button is PRESSED\n");
+		gpio_set_value(GPIO_LED1_NUMBER, 1);
+	}	
+	else 
+	{
+		sprintf(text,"Button is RELEASED\n");
+		gpio_set_value(GPIO_LED1_NUMBER, 0);
+	}
 
 	if(text[cnt]!='\0') {
                 copy_to_user(user_buf, &text[cnt], 1);
@@ -131,16 +141,25 @@ static int button1_init(void){
 	return -1;
 	}
 
-	if(gpio_request(GPIO_NUMBER, "button1")){
-                printk( KERN_ALERT "gpio request failed\n" );
+	if(gpio_request(GPIO_BUTTON1_NUMBER, "button1")){
+                printk( KERN_ALERT "gpio_button1 request failed\n" );
                 device_destroy( button1.class, button1.dev_t );
                 class_destroy( button1.class );
                 unregister_chrdev_region( button1.dev_t, 1 );
                 return -1;
         }
-	gpio_direction_input(GPIO_NUMBER);
-	gpio_export(GPIO_NUMBER, true);
+	gpio_direction_input(GPIO_BUTTON1_NUMBER);
+	gpio_export(GPIO_BUTTON1_NUMBER, true);
 	
+	if(gpio_request(GPIO_LED1_NUMBER, "led1")){
+                printk( KERN_ALERT "gpio_led1 request failed\n" );
+                device_destroy( button1.class, button1.dev_t );
+                class_destroy( button1.class );
+                unregister_chrdev_region( button1.dev_t, 1 );
+                return -1;
+        }
+	gpio_direction_output(GPIO_LED1_NUMBER, 0);
+	gpio_export(GPIO_LED1_NUMBER, true);
 
 	printk(KERN_INFO "button1: Successfully registered driver with kernel.\n");
 
@@ -153,7 +172,8 @@ static void button1_exit(void){
 /*	When exit is called, clean up kernel data*/
 	cdev_del(&button1.cdev);
 	unregister_chrdev_region(button1.dev_t, 1);	
-	gpio_free(GPIO_NUMBER);
+	gpio_free(GPIO_BUTTON1_NUMBER);
+	gpio_free(GPIO_LED1_NUMBER);
 
 	printk(KERN_WARNING "button1: Device unregistered\n");
 }
